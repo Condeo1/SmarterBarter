32ed4cbdaf09b0bdc3cdeeaa2221bbcc9dbfeec5
codylm <codylm@users.noreply.github.com>
2017-02-22 16:05:20 -0600

    database stuff

--- mysite/home/admin.py
+++ mysite/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from home.models import Customers, CustomerServices
 
-# Register your models here.
+admin.site.register(Customers)
+admin.site.register(CustomerServices)
--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Customers(models.Model):
+    customerID = models.CharField(max_length = 7)
+    firstName = models.CharField(max_length = 200)
+    lastName = models.CharField(max_length = 200)
+    zipCode = models.IntegerField(default = 0)
+    email = models.CharField(max_length = 500)
+    
+    def __str__(self):
+        return self.firstName
+    
+class CustomerServices(models.Model):
+    customers = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    servicesID = models.CharField(max_length = 60)
+    needsID = models.CharField(max_length = 60)
+    
+    def __str__(self):
+        return self.customers.firstName
+
+class Login(models.Model):
+    customers = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    userName = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 20)
+    
+    def __str__(self):
+        return self.customers.firstName
+
+    
+
