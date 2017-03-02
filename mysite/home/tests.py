97b3314c583d71181932e10c7d52ec9db85b1238
codylm <codylm@users.noreply.github.com>
2017-03-02 12:55:15 -0600

    added more tests

--- mysite/home/tests.py
+++ mysite/home/tests.py
@@ -35,3 +35,32 @@ class CustomerServicesTestCase(TestCase):
     def test_customerOneOffersServiceZero(self):
         cusZero = CustomerServices.objects.get(customers = Customers.objects.get(customerID = "00"))
         self.assertEqual(cusZero.servicesID, "00")
+        
+    def test_addServiceToCustomerZero(self):
+        newService = "02"
+        cusZero = CustomerServices.objects.get(customers = Customers.objects.get(customerID = "00"))
+        cusZero.servicesID += "," + newService
+        cusZero.save()
+        idArray = cusZero.servicesID.split(",")
+        for i in range(0, len(idArray)):
+            if(idArray[i] == newService):
+                selectedService = idArray[i]
+        self.assertEqual(selectedService, "02")
+        self.assertEqual(cusZero.servicesID, "00,02")
+        
+    #def test_databasePreservesTheDataAcrossTests(self):
+     #   cusZero = CustomerServices.objects.get(customers = Customers.objects.get(customerID = "00"))
+      #  self.assertEqual(cusZero.servicesID, "00,02")
+
+    def test_removeServiceFromCustomerOne(self):
+        serviceToRemove = "00"
+        newService = "02"
+        cusZero = CustomerServices.objects.get(customers = Customers.objects.get(customerID = "00"))
+        cusZero.servicesID += "," + newService
+        self.assertEqual(cusZero.servicesID, "00,02")
+        idArray = cusZero.servicesID.split(",")
+        idArray.remove(serviceToRemove)
+        cusZero.servicesID = ""
+        for i in range(0, len(idArray)):
+            cusZero.servicesID += idArray[i]
+        self.assertEqual(cusZero.servicesID, "02")
