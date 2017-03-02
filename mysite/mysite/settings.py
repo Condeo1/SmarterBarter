7a0f2ff29065610802bc144174945738431b1e0f
Tyler Brown <tylerbro54@yahoo.com>
2017-03-02 10:29:37 -0600

    created a method to reset key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = ''
+with open('tempKEY.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 DEBUG = True
 
