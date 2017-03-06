c042a99282c9c50cbf10f2155fcb1b6fd06925ed
codylm <codylm@users.noreply.github.com>
2017-03-06 01:31:17 -0600

    attempted to create a basic register/login system. I dont have the html knowledge to make the actual page properly. I dont know if itll work and as far as i know it doesnt connect up to the customer info db in any way.

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
     url(r'^faq/$', views.faq, name='faq'),
     url(r'^tos/$', views.tos, name='tos'),
     url(r'^contact/$', views.contact, name='contact'),
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
 def index(request):
     return render(request, 'home/home.html')
@@ -11,3 +14,35 @@ def tos(request):
     
 def contact(request):
     return render(request, 'home/contact.html')
+    
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'home/register.html'
+    
+    #display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+        
+    #process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+        
+        if form.is_valid():
+            user = form.save(commit=False)
+            
+            #cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+            
+            #returns User objects if credentials are correct
+            user = authenticate(username=username, password=password)
+            
+            if(user != None):
+                if(user.is_active):
+                    login(request, user)
+                    return redirect('home/home.html')
+                    
+        return render(request, self.template_name, {'form': form})
