9a1a2cc97b5732d343617dc70f4b0be031809e69
Jonnybeecham <Jonnybeecham@yahoo.com>
2017-03-09 16:08:52 -0600

    Changes to the Database
    This made there be a sort of tree of info like this:
    
    user stores the customers name, username, password, email.
    
    customer now stores the zipcode and bio.
    
    customer service is still the same.

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,13 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Customer(models.Model):
-    customerID = models.CharField(max_length = 7)
-    firstName = models.CharField(max_length = 200)
-    lastName = models.CharField(max_length = 200)
-    userName = models.CharField(max_length = 200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     zipCode = models.IntegerField(default = '55555')
-    email = models.EmailField(max_length = 500)
-    password = models.CharField(max_length = 20)
     bio = models.TextField(max_length = 1500)
     
     def __str__(self):
@@ -17,12 +13,6 @@ class CustomerService(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     servicesID = models.CharField(max_length = 60)
     needsID = models.CharField(max_length = 60)
-    
-    def __str__(self):
-        return self.customer.userName
-
-class Login(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.customer.userName
