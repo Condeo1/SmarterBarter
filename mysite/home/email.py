56a18612b0c3f3ea7ca08f5c89db68ed2c2e8b49
Jonny Beecham <jbeecham@wrightmediacorp.com>
2017-03-29 09:42:15 -0500

    email stuff for contact us.

--- mysite/home/email.py
+++ mysite/home/email.py
@@ -1,16 +1,26 @@
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
+from home.models import *
+from secret_settings import *
 
-def send_email(request):
-    subject = request.POST.get('subject', '')
-    message = request.POST.get('message', '')
-    if subject and message:
-        try:
-            send_mail(subject, message, ['Smarterbarter'], ['admin@example.com'])
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return HttpResponseRedirect('/contact/')
-    else:
-        # In reality we'd use a form class
-        # to get proper validation errors.
-        return HttpResponse('Make sure all fields are entered and valid.')
+@csrf_protect
+def contact(request):
+    form = ContactMessage(request.POST or None)
+    if form.is_valid():
+        form_message = form.cleaned_data.get("emailMessage")
+        subject = 'Site Contact form'
+        from_email = secret_settings.EMAIL_HOST_USER
+        to_email = secret_settings.EMAIL_HOST_USER
+        contact_message = "%s: %s via %s"(
+            "SmarterBarter",
+            form_message,
+            form_email)
+        send_mail(subject,
+                 contact_message,
+                 from_email,
+                 to_email,
+                 fail_silently = False)
+    context = {
+        "form": form,
+    }    
+    return render(request, "contact.html", context)
